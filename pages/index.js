298a29461c961c167cfb4fdda75b6fc632adc4ce
Joshhortt <joshhortt@yahoo.com>
2022-09-27 14:22:59 +0100

    add product array map

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,21 @@ import React from 'react'
 const Home = () => {
   return (
     <>
+    
     HeroBanner
 
     <div>
       <h2>Produtos em Destaque</h2>
       <p>Moedas das mais variadas épocas</p>
     </div>
+
+    <div>
+      {[ 'Product 1', 'Product 2'].map(
+        (product) => product)
+      }
+    </div>
+
+    Footer
 
     </>
   )
