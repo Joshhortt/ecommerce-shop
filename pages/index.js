b7c17c5090dc77e0aa4c7ec5400820975e7f8792
Joshhortt <joshhortt@yahoo.com>
2022-09-28 16:00:07 +0100

    add HeroBanner hooked up to Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
 
-import { Product, FooterBanner, HeroBanner } 
-from '../components';
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-
-
-const Home = () => {
-  return (
-    <>
-    
-    <HeroBanner />
-
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Produtos em Destaque</h2>
       <p>Moedas portuguesas e Internacionais das mais variadas épocas</p>
     </div>
 
     <div className="products-container">
-      {[ 'Product 1', 'Product 2'].map(
-        (product) => product)
-      }
+      {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
-    <FooterBanner />
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
-    </>
-  )
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
-export default Home
+export default Home;
