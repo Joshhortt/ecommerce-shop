37c057a747e60122870dc352db4d0cdeec51c795
Joshhortt <joshhortt@yahoo.com>
2022-10-11 16:35:01 +0100

    add footerBanner hooked up to Sanity

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+
+import { urlFor } from '../lib/client';
+
 
 const FooterBanner = () => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+        <p>{FooterBanner.discount}</p>
+        <h3>{FooterBanner.largeText1}</h3>
+        <h3>{FooterBanner.largeText2}</h3>
+        </div>
+        
+        <div className='right'>
+        </div>
+      </div>
+    </div>
   )
 }
 
