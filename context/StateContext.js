9b20e0eec74b4d54739cbdc6af62ed1decccf1c4
Joshhortt <joshhortt@yahoo.com>
2022-10-17 17:49:00 +0100

    add Cart context logic to Navbar  component

--- context/StateContext.js
+++ context/StateContext.js
@@ -51,6 +51,7 @@ const onAdd = (product, quantity) => {
     <Context.Provider
       value={{
         showCart,
+        setShowCart, 
         cartItems,
         totalPrice,
         totalQuantities,
