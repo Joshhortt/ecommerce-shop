f988ede9cedcd21585b8795eb0de136df8be8db2
Joshhortt <joshhortt@yahoo.com>
2022-10-17 18:54:54 +0100

    add subtotal + pay with stripe btn

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -75,6 +75,20 @@ const Cart = () => {
           ))}
         </div>
 
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>€{totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick="">
+                Pagar com Stripe
+              </button>
+            </div>
+          </div>
+        )}
+
       </div>
     </div>
   )
