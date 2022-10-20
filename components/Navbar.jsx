2814bec947e45fe403c2143d0dd7236822f4366e
Joshhortt <joshhortt@yahoo.com>
2022-10-20 22:36:49 +0100

    add strong tag to Logo Link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href='/'>PHONIX</Link>
+        <Link href='/'><strong>PHONIX</strong></Link>
       </p>
 
       <button type='button'
