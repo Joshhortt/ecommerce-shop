0bccaff1a2bdc526f2eb3e45d4d207bec97da31d
Joshhortt <joshhortt@yahoo.com>
2022-10-21 19:01:20 +0100

    implement Product details with getStatic Paths/Props

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,23 +8,33 @@ const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
   const {decQty, incQty, qty, onAdd } = useStateContext();
+
+  const handleBuyNow = () => {
+		onAdd(product, qty)
+
+		setShowCart(true)
+	}
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} className="product-detail-image" />
+            <img 
+            className="product-detail-image"
+            src={urlFor(image && image[index])} 
+             />
           </div>
-          <div className="small-images-container">
-            {image?.map((item, i) => (
-              <img 
-                key={i}
-                src={urlFor(item)}
-                className={i === index ? 'small-image selected-image' : 'small-image'}
-                onMouseEnter={() => setIndex(i)}
+            <div className="small-images-container">
+              {image?.map((item, i) => (
+                <img 
+                  key={i}
+                  src={urlFor(item)}
+                  className={i === index ? 'small-image selected-image' : 'small-image'}
+                  onMouseEnter={() => setIndex(i)}
               />
             ))}
-          </div>
+            </div>
           </div>
 
           <div className="product-detail-desc">
@@ -43,9 +53,9 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <h4>Detalhes: </h4>
           <p>{details}</p>
-          <p className="price">€{price}</p>
+          <p className="price">{price} €</p>
           <div className="quantity">
-            <h3>Quantidade:</h3>
+            <h3>Quantidade: </h3>
             <p className="quantity-desc">
               <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
               <span className="num">{qty}</span>
@@ -54,8 +64,14 @@ const ProductDetails = ({ product, products }) => {
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Adicionar ao carrinho</button>
-            <button type="button" className="buy-now" onClick="">Comprar agora</button>
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
+              {' '}
+              Adicionar
+              </button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
+            {' '}
+              Comprar
+              </button>
           </div>
         </div>
       </div>
@@ -86,20 +102,20 @@ export const getStaticPaths = async () => {
 
   const products = await client.fetch(query);
 
-  const paths = products.map((product) => ({
+  const paths = products.map(product => ({
     params: { 
-      slug: product.slug.current
-    }
-  }));
+      slug: product.slug.current,
+    },
+  }))
 
   return {
     paths,
-    fallback: 'blocking'
+    fallback: 'blocking',
   }
 }
 
 export const getStaticProps = async ({ params: { slug }}) => {
-  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`
   const productsQuery = '*[_type == "product"]'
   
   const product = await client.fetch(query);
